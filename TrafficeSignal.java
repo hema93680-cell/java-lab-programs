a06587e2fc033becdca4ecf6d8c5580425fa541a
hema93680-cell <hema93680@gmail.com>
2026-03-08 19:13:42 +0530

    Add TrafficSignal class with main method

---
+++ TrafficeSignal.java
@@ -0,0 +1,9 @@
+public class TrafficSignal {
+    public static void main(String[] args) {
+
+        System.out.println("Red Light - Stop");
+        System.out.println("Yellow Light - Ready");
+        System.out.println("Green Light - Go");
+
+    }
+}
